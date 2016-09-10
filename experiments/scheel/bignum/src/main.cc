f0a7c0527d03217a1d1a9eff556ca7a1ce821ea2
Alexander Scheel <scheel@iastate.edu>
2016-09-10 14:20:01 -0500

    [bignum] Fixed memory leaks

--- experiments/scheel/bignum/src/main.cc
+++ experiments/scheel/bignum/src/main.cc
@@ -8,44 +8,59 @@
 
 int main() {
   printf("a = 0xFFFFFFFFFFFFFFFF:\n");
-  BigInt a = BigInt(0xFFFFFFFFFFFFFFFF);
-  a.print();
+  BigInt *a = new BigInt(0xFFFFFFFFFFFFFFFF);
+  a->print();
 
   printf("b = 0x10:\n");
-  BigInt b = BigInt(0x10);
-  b.print();
+  BigInt *b = new BigInt(0x10);
+  b->print();
 
-  printf("c = ba\n");
+  printf("c = b+a\n");
   // Appending:
-  BigInt c = b.add(a);
-  c.print();
+  BigInt *c = b->add(a);
+  c->print();
 
   printf("c = -c: \n");
-  c.negate();
-  c.print();
+  c->negate();
+  c->print();
 
   printf("d = ba\n");
-  BigInt d = b.add(a);
-  d.print();
+  BigInt *d = b->add(a);
+  d->print();
 
-  printf("d = d + c\n");
-  d = d.add(c);
-  d.print();
+  printf("j = d + c\n");
+  BigInt *j = d->add(c);
+  j->print();
 
   printf("e = -2\n");
-  BigInt e = BigInt(0x02);
-  e.negate();
-  e.print();
+  BigInt *e = new BigInt(0x02);
+  e->negate();
+  e->print();
 
   printf("f = 1\n");
-  BigInt f = BigInt(0x01);
-  f.print();
+  BigInt *f = new BigInt(0x01);
+  f->print();
 
   printf("g = e + f\n");
-  BigInt g = e.add(f);
-  g.print();
+  BigInt *g = e->add(f);
+  g->print();
 
   printf("h = g + f\n");
-  BigInt h = g.add(f);
-  h.print();
+  BigInt *h = g->add(f);
+  h->print();
+
+  printf("i = e - f\n");
+  BigInt *i = e->subtract(f);
+  i->print();
+
+  delete a;
+  delete b;
+  delete c;
+  delete d;
+  delete e;
+  delete f;
+  delete g;
+  delete h;
+  delete i;
+  delete j;
 }
