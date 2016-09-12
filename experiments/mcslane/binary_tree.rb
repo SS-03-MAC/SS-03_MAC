8d27e1527d44cc2a6560729f7a0a75ab6931b074
Matthew Slane <mcslane@iastate.edu>
2016-09-11 23:02:50 -0500

    created basic binary tree structure

---
+++ experiments/mcslane/binary_tree.rb
@@ -0,0 +1,75 @@
+module BinarySearchTree
+  class BinaryTreeNode
+    attr_reader :data
+    attr_accessor :left, :right, :parent
+
+    def initialize(data)
+      @data = data
+    end
+
+    def insert(v)
+      case data <=> v
+      when 1 then insert_left(v)
+      when -1 then insert_right(v)
+      when 0 then false
+      end
+    end
+
+    def to_s
+      return "(#{data} #{left} #{right})"
+    end
+
+    private
+
+    def insert_left(v)
+      if left
+        left.insert(v)
+      else
+        self.left = BinaryTreeNode.new(v)
+      end
+    end
+
+    def insert_right(v)
+      if right
+        right.insert(v)
+      else
+        self.right = BinaryTreeNode.new(v)
+      end
+    end
+  end
+
+  class BinaryTree
+    attr_reader :root
+    attr_accessor :root
+
+    def initialize(data=nil)
+      if data.nil?
+        @root = nil
+      else
+        @root = BinaryTreeNode.new(data)
+      end
+    end
+
+    def insert(data)
+      if data.nil?
+        return false
+      elsif @root.nil?
+        puts "new root"
+        @root = BinaryTreeNode.new(data)
+      else
+        puts "inserting data"
+        @root.insert(data)
+      end
+    end
+
+    def to_s
+      return @root.to_s
+    end
+  end
+end
+
+tree = BinarySearchTree::BinaryTree.new()
+tree.insert(10)
+tree.insert(15)
+tree.insert(5)
+puts tree
