49652b0604229c6f88420ec131e0e8957c135a55
Matthew Burket <mburket@iastate.edu>
2016-09-13 15:30:31 -0500

    CGI example work

---
+++ experiments/mburket/cSharpExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           string contentLength = System.Environment.GetEnvironmentVariable("HTTP_COOKIE");
+           Console.WriteLine("Content-Type:text/html;charset=utf-8");
+           Console.WriteLine(contentLength);
+        }
+    }
+}
