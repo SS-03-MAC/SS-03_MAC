db948d673946cd76314461202e30d4e6bfe55d8b
Matthew Burket <mburket@iastate.edu>
2016-09-14 16:10:22 -0500

    Reading forms example

---
+++ experiments/mburket/cSharpPOST/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int contentLength = int.Parse(System.Environment.GetEnvironmentVariable("CONTENT_LENGTH"));
+            byte[] input = new byte[contentLength];
+            using (var stdin = Console.OpenStandardInput()) {
+            int a = stdin.Read(input, 0, input.Length);
+            Console.WriteLine("Content-Type:text/html;charset=utf-8\n");
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(input));
+            Console.WriteLine(a);
+        }
+    }
+}
+}
