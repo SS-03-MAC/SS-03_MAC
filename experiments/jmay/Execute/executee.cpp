34d5bf3c30c8a36796953e4febbfa9e4124daa51
Joel May <joel.may@live.com>
2016-09-14 12:05:27 -0500

    Execute

--- experiments/jmay/Execute/executee.cpp
+++ experiments/jmay/Execute/executee.cpp
@@ -4,5 +4,8 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << std::endl;
     }
+    long addr;
+    sscanf(argv[1], "%ld", &addr);
+    printf("%s", (char *) addr);
     return 0;
 }
--- experiments/jmay/Execute/executer.cpp
+++ experiments/jmay/Execute/executer.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    // exececute other binary
+    char* test;
+    malloc(10000);
+    test = (char*) malloc(20);
+    sprintf(test, "This is only a test");
+    char testAddr[20];
+    sprintf(testAddr, "%ld", (long) test);
+    char* const newargv[] = {(char *) "executee.exe", (char *const) testAddr, NULL};
+    execv("./executee.exe", newargv);
     return 0;
 }
