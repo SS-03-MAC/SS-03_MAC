60ee26bf65cbaca76c05fa06ede3433428159106
Matthew Burket <mburket@iastate.edu>
2016-09-14 15:18:34 -0500

    Make the C# work

--- experiments/mburket/cSharpExamples/Program.cs
+++ experiments/mburket/cSharpExamples/Program.cs
@@ -8,8 +8,15 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
            string contentLength = System.Environment.GetEnvironmentVariable("HTTP_COOKIE");
-           Console.WriteLine("Content-Type:text/html;charset=utf-8");
-           Console.WriteLine(contentLength);
+           Console.WriteLine("Content-Type:text/html;charset=utf-8\n");
+           Console.WriteLine("<html>");
+           Console.WriteLine("<head>");
+           Console.WriteLine("<title>Hello World</title>");
+           Console.WriteLine("</head>");
+           Console.WriteLine("<body>");
+           Console.WriteLine("<h1>Hello World</h1>");
+           Console.WriteLine("</body>");
+           Console.WriteLine("</html>");
         }
     }
 }
