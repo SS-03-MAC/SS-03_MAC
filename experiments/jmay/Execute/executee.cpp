9e0a270787624a8af25d48a676d75c0cdaba651f
Joel May <joel.may@live.com>
2016-09-19 18:32:47 -0500

    Execution and piping work

--- experiments/jmay/Execute/executee.cpp
+++ experiments/jmay/Execute/executee.cpp
@@ -5,8 +5,17 @@ int main(int argc, char* argv[]) {
 //        std::cout << argv[i] << std::endl;
 //    }
     char input[200];
-    gets(input);
+    //gets(input);
+    int i = 0;
+    do {
+        int read;
+        read = getchar();
+        printf("%d '%c'\n", read, (char) read);
+        input[i] = (char) read;
+        //fflush(stdout);
+    } while (input[i++] != EOF && i < 199);
+    input[i - 1] = 0;
     printf("Just in: %s", input);
-
+fflush(stdout);
     return 0;
 }
--- experiments/jmay/Execute/fork.cpp
+++ experiments/jmay/Execute/fork.cpp
@@ -14,6 +14,9 @@ int main(int argc, char* argv[]) {
     printf("%d\n", f);
     if (f == 0) {
         // Child
+        close(pipeout[1]);
+        close(pipein[0]);
+
         dup2(pipeout[0], STDIN_FILENO);
         dup2(pipein[1], STDOUT_FILENO);
         dup2(pipein[1], STDERR_FILENO);
@@ -21,10 +24,13 @@ int main(int argc, char* argv[]) {
         exit(1);
     } else {
         // Parent
+        close(pipeout[0]);
+        close(pipein[1]);
+
         char buf[200];
-        sprintf(buf, "I'm testing stdin\n");
+        sprintf(buf, "I'm testing stdin");
         write(pipeout[1], buf, strlen(buf));
-        // fflush?
+        close(pipeout[1]);
         ssize_t r = read(pipein[0], buf, sizeof(buf));
         buf[r] = '\0';
         printf("Received: %s\n", buf);
