0a1d5720040dc6664167ae913295794e19500082
Alexander Scheel <scheel@iastate.edu>
2016-09-20 11:38:37 -0500

    [EduTLS] {bignum} StdC11?

--- EduTLS/tests/bignum/make.sh
+++ EduTLS/tests/bignum/make.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-clang-format -style="{BasedOnStyle: llvm, ColumnLimit: 120}" -i ./*.cc ./*.h
+if [ "$(which clang-format)" != "" ]; then
+	clang-format -style="{BasedOnStyle: llvm, ColumnLimit: 120}" -i ./*.cc ./*.h
+fi
 
 rm -rf ./a.out.dSYM ./a.out
 
-g++ ./main.cpp ../../src/bignum/*.cpp -Werror -Wall -Wextra -O0 -ggdb
+g++ ./main.cpp ../../src/bignum/*.cpp -Werror -Wall -Wextra -O0 -ggdb -std=c++11
