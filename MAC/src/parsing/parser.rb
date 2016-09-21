90c46844864e6d21b8764e12b20e8e46778a05a4
Matthew Slane <mcslane@iastate.edu>
2016-09-21 10:39:14 -0500

    started parser

---
+++ MAC/src/parsing/parser.rb
@@ -0,0 +1,11 @@
+require "yaml"
+
+module Parser
+  def parse_file path
+    parsed = begin
+      YAML.load(File.open(path))
+    rescue ArgumentError => e
+      puts "Could nor parse YAML: #{e.message}" 
+    end
+  end
+end
