4ef4d8770f8689f66c73984f8a610c0a5f93f5de
Matthew Slane <mcslane@iastate.edu>
2016-09-21 19:09:44 -0500

    Finalizes single file unit tests, all passing

--- MAC/src/parsing/testing/tc_parser.rb
+++ MAC/src/parsing/testing/tc_parser.rb
@@ -1,12 +1,21 @@
-require "./../parser"
+require "./../parser.rb"
 require "test/unit"
+
+include Parser
 
 class TestParser < Test::Unit::TestCase
 
-  def test_success
+  def test_single_success
+    expected = [{"name"=>"Billy Bob", "state"=>"Arkansas", "age"=>38}]
+    path = "./data/example_yaml.yml"
+    parsed = Parser::parse_file(path)
+    assert_equal(parsed, expected)
   end
 
-  def test_failure
+  def test_single_failure
+    path = "./data/lorem_ipsum.txt"
+    parsed = Parser::parse_file(path)
+    assert_nil(parsed)
   end
 
 end
