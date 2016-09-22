5d53c85e9d88d1765c3bb519f2098e0ae1e0a9cc
Matthew Slane <mcslane@iastate.edu>
2016-09-21 19:27:51 -0500

    stubbed out directory searching method

--- MAC/src/parsing/parser.rb
+++ MAC/src/parsing/parser.rb
@@ -1,8 +1,9 @@
 require "yaml"
 
 module Parser
-  def parse_file path
 
+  #checks if a file could be YAML and parses it
+  def parse_file path
     #checks if the file is valid YAML (includes JSON), if not, returns nil
     accepted_formats = [".yml", ".yaml", ".json"]
     ext = File.extname(path)
@@ -18,5 +19,21 @@ module Parser
       nil
     end
     return parsed
+  end
+
+  #finds all YAML files in a directory and parses them into a hash of Objects,
+  #keys are file names, values are parsed YAML objects
+  def parse_directory dir_path
+    result = {}
+
+    Dir.foreach(dir_path) do |item|
+      #skips any other directories found, including '.' and '..'
+      next if item == '.' or item == '..'
+      if File.directory?(item)
+        #recursively calls parse_directory, adding more YAML to our hash
+      else
+        #tries to parse file, if parse_file(item) returns nil, don't include value
+      end
+    end
   end
 end
