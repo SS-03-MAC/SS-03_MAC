00de091f0fb0bb193f6606f8113f347650d28d56
Matthew Slane <mcslane@iastate.edu>
2016-09-21 19:05:05 -0500

    added extension checks to parser

--- MAC/src/parsing/parser.rb
+++ MAC/src/parsing/parser.rb
@@ -2,12 +2,21 @@ require "yaml"
 
 module Parser
   def parse_file path
+
+    #checks if the file is valid YAML (includes JSON), if not, returns nil
+    accepted_formats = [".yml", ".yaml", ".json"]
+    ext = File.extname(path)
+    if !accepted_formats.contains? ext
+      return nil
+    end
+
+    #tries to parse YAML, if something goes wrong, displays error, returns nil 
     parsed = begin
       YAML.load(File.open(path))
     rescue ArgumentError => e
-      puts "Could nor parse YAML: #{e.message}" 
+      puts "Could not parse YAML from file #{path}" 
+      nil
     end
-
     return parsed
   end
 end
