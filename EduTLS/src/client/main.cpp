f0e40760ef7500d5fd907764bb82b94412b63121
Alexander Scheel <scheel@iastate.edu>
2016-09-26 09:04:06 -0500

    [EduTLS] {sha2-256} Implementation

--- EduTLS/src/client/main.cpp
+++ EduTLS/src/client/main.cpp
@@ -1,5 +1,8 @@
 #include "../crypto/hash/hash.h"
 #include "../crypto/hash/md.h"
+#include "../crypto/hash/md5.h"
+#include "../crypto/hash/sha1.h"
+#include "../crypto/hash/sha2-256.h"
 #include "../crypto/symmetric/aes.h"
 #include "../crypto/symmetric/symmetric.h"
 
