9258a981fc55a4e221179c67355a97d7b911ee5f
Matthew Slane <mcslane@iastate.edu>
2016-09-27 13:50:06 -0500

    Added abstract class for types

---
+++ MAC/src/MAC/types/BaseType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public abstract class BaseType : IComparable, IEquitable<BaseType>, ISerializable
+{
+	public BaseType()
+	{
+
+	}
+
+    public bool Validate();
+
+    public int compareTo(Object other);
+
+    public int equals(BaseType other);
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context);
+}
