8ec732518889005c3a7acf96288992e252a6eeb0
Matthew Slane <mcslane@iastate.edu>
2016-09-27 15:50:59 -0500

    added documentation

--- MAC/src/MAC/types/BaseType.cs
+++ MAC/src/MAC/types/BaseType.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-public abstract class BaseType : IComparable, IEquitable<BaseType>, ISerializable
+/*
+ * Base abstract classfor MAC types. Each MAC type will inherit this class. 
+ * Children of this class are used as wrapper classes for data, such that
+ * all data can be fund easily, and to make sure data is comparable, 
+ * equitable, and serializable
+ */
+public abstract class BaseType : IComparable<BaseType>, IEquitable<BaseType>,
+                                 ISerializable
 {
-	public BaseType()
-	{
+	public BaseType() { }
 
-	}
-
+    //function to validate data for the given object is in the correct format
+    //independantly inplemented by each child class
     public bool Validate();
 
-    public int compareTo(Object other);
+    //function inherited from IComparable
+    public int compareTo(BaseType other);
 
+    //function inherited from IEquitable
     public int equals(BaseType other);
 
+    //function inherited from ISerializable
     public void GetObjectData(SerializationInfo info, StreamingContext context);
 }
