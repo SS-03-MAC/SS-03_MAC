0a2e538beca7b3893608ad55d9bd4027900a7bb5
Matthew Burket <mburket@iastate.edu>
2016-09-28 08:33:53 -0500

    Started Integer type

---
+++ MAC/src/MAC/types/Integer.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Runtime.Serialization;
+
+
+ namespace MAC.Types {
+     /// <summary>
+     /// Stores and vaildates Phyiscal Address
+     /// </summary>
+     public class Integer : BaseType {
+        /// <summary>
+        /// This will check for:
+        /// - if is an intger
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Compare the interger
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks equality of the interger
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Equals(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Should return the interger
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
+     }
+ }
