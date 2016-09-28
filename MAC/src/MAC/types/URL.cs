89b5bc1768a399225766ba82348ae3ebf757468f
Matthew Burket <mburket@iastate.edu>
2016-09-28 08:58:24 -0500

    Started URL Type

---
+++ MAC/src/MAC/types/URL.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Runtime.Serialization;
+
+
+ namespace MAC.Types.User {
+     /// <summary>
+     /// Stores and vaildates URLs
+     /// Stored as SQL type varchar
+     /// </summary>
+     public class URL : BaseType {
+        /// <summary>
+        /// This will check if is a URL
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Compare the URL
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks equality of the URL
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Equals(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Should return a string of the URL
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
+     }
+ }
