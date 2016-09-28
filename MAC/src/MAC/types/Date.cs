2aaeefaee40fb8e246482240b367c2b64b05b706
Matthew Burket <mburket@iastate.edu>
2016-09-28 08:31:13 -0500

    Added date type

---
+++ MAC/src/MAC/types/Date.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Runtime.Serialization;
+
+
+ namespace MAC.Types {
+     /// <summary>
+     /// Stores and vaildates dates
+     /// Stored as a SQL date
+     /// eg 08/28/2016
+     /// </summary>
+     public class Date : BaseType {
+        /// <summary>
+        /// This will check for:
+        /// Will check if the date is format can interpered as a date
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Compare the Date
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks equality of the strings
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Equals(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Should return the string
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
+     }
+ }
