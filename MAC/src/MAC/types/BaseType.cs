947dde8d7f0417ee9eadc56ca87d353d99cfa99d
Matthew Burket <mburket@iastate.edu>
2016-09-27 19:28:44 -0500

    Added namesspace to Base Type

--- MAC/src/MAC/types/BaseType.cs
+++ MAC/src/MAC/types/BaseType.cs
@@ -2,26 +2,29 @@
 using System.Runtime.Serialization;
 
 /*
- * Base abstract classfor MAC types. Each MAC type will inherit this class. 
+ * Base abstract classfor MAC types. Each MAC type will inherit this class.
  * Children of this class are used as wrapper classes for data, such that
- * all data can be fund easily, and to make sure data is comparable, 
+ * all data can be fund easily, and to make sure data is comparable,
  * equitable, and serializable
  */
-public abstract class BaseType : IComparable<BaseType>, IEquitable<BaseType>,
-                                 ISerializable
-{
-	public BaseType() { }
 
-    //function to validate data for the given object is in the correct format
-    //independantly inplemented by each child class
-    public bool Validate();
+ namespace MAC.Types {
+    public abstract class BaseType : IComparable<BaseType>, IEquitable<BaseType>,
+                                     ISerializable
+    {
+    	public BaseType() { }
 
-    //function inherited from IComparable
-    public int compareTo(BaseType other);
+        //function to validate data for the given object is in the correct format
+        //independantly inplemented by each child class
+        public bool Validate();
 
-    //function inherited from IEquitable
-    public int equals(BaseType other);
+        //function inherited from IComparable
+        public int CompareTo(BaseType other);
 
-    //function inherited from ISerializable
-    public void GetObjectData(SerializationInfo info, StreamingContext context);
+        //function inherited from IEquitable
+        public int Equals(BaseType other);
+
+        //function inherited from ISerializable
+        public void GetObjectData(SerializationInfo info, StreamingContext context);
+    }
 }
