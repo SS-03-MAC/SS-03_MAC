5456e6f1b3c2bbf13f73c9b8f7d781d97785f8d9
Matthew Burket <mburket@iastate.edu>
2016-09-27 22:18:22 -0500

    Added User Address Type

---
+++ MAC/src/MAC/types/Address.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Runtime.Serialization;
+
+
+ namespace MAC.Types.User {
+     /// <summary>
+     /// Stores and vaildates 
+     /// </summary>
+     public class Address : BaseType {
+        /// <summary>
+        /// This will check for:
+        /// Street, and Zip
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Compare address
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks equality of the address
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Equals(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Should return the address as a string
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
+     }
+ }
