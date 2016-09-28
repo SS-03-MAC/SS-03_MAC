3531092b10ae3b9b81bf31064133a00c348712c1
Matthew Burket <mburket@iastate.edu>
2016-09-28 08:32:25 -0500

    Started datetime type

---
+++ MAC/src/MAC/types/DateTime.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Runtime.Serialization;
+
+
+ namespace MAC.Types {
+     /// <summary>
+     /// Stores and vaildates times
+     /// Stored as a SQL time
+     /// eg 08/28/2016
+     /// </summary>
+     public class DateTime : BaseType {
+        /// <summary>
+        /// This will check for:
+        /// Will check if the time is format can interpered as a datetime
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate() {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Compare the time
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks equality of the time
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int Equals(BaseType other) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Should return the string
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) {
+            throw new NotImplementedException();
+        }
+     }
+ }
