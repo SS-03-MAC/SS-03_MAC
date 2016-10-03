cd795dcb88b18eab2b8fff27c940792d920470d0
Alexander Scheel <scheel@iastate.edu>
2016-10-03 12:38:07 -0500

    [EduTLS] {entropy} Mac OS X

--- EduTLS/src/crypto/entropy.h
+++ EduTLS/src/crypto/entropy.h
@@ -15,8 +15,55 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+
+#ifdef __APPLE__
+
+static inline void edutls_rand_read_raw(uint8_t *output) {
+  /*
+   Per: https://developer.apple.com/library/content/documentation/Security/Conceptual
+        /cryptoservices/RandomNumberGenerationAPIs/RandomNumberGenerationAPIs.html
+  */
+  FILE *fp = fopen("/dev/random", "r");
+  size_t i = 0;
+
+  if (!fp) {
+    perror("randgetter");
+    exit(-1);
+  }
+
+  for (i = 0; i < 255; i++) {
+    output[i] = fgetc(fp);
+  }
+
+  fclose(fp);
+}
+
+static inline void edutls_rand_bytes(uint8_t *output, size_t count) {
+  size_t o_p = 0;
+  size_t b_p = 0;
+  uint8_t buffer[255];
+  edutls_rand_read_raw(buffer);
+
+  while (o_p < count) {
+    output[o_p] = buffer[b_p];
+    o_p += 1;
+    b_p += 1;
+
+    if (o_p != count) {
+      if (b_p == 255) {
+        b_p = 0;
+
+        edutls_rand_read_raw(buffer);
+      }
+    }
+  }
+}
+#endif
 
 #ifdef __linux__
+#include <cstdio.h>
 #include <errno.h>
 #include <exception>
 #include <linux/random.h>
@@ -33,12 +80,16 @@ static inline void edutls_rand_check(uint8_t *buffer, int result, int err) {
       edutls_rand_check(buffer, result, errno);
       break;
     case EINVAL:
-      throw EINVAL;
+      perror("[edutls_rand_check] Invalid flags supplied to getrandom()");
+      exit(-1);
       break;
     case EFAULT:
-      throw EFAULT;
+      perror("[edutls_rand_check] Fault in getrandom()");
+      exit(-1);
       break;
     default:
+      perror("[edutls_rand_check] Unknown errno number");
+      exit(-1);
       throw - 1;
     }
   } else if (result != 255) {
@@ -70,5 +121,4 @@ static inline void edutls_rand_bytes(uint8_t *output, size_t count) {
     }
   }
 }
-
 #endif
