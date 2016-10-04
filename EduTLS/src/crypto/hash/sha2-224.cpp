4c0980a68f049d6ec59ac98c5610c53ea30bb8d9
Alexander Scheel <scheel@iastate.edu>
2016-10-03 19:55:35 -0500

    [EduTLS] Formatting

--- EduTLS/src/crypto/hash/sha2-224.cpp
+++ EduTLS/src/crypto/hash/sha2-224.cpp
@@ -51,8 +51,8 @@ void sha2_224::core() {
       0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
   for (t = 0; t < 16; t++) {
-    w[t] = (((uint32_t) this->block[t * 4 + 0]) << 24) | (((uint32_t) this->block[t * 4 + 1]) << 16) |
-           (((uint32_t) this->block[t * 4 + 2]) << 8) | (((uint32_t) this->block[t * 4 + 3]) << 0);
+    w[t] = (((uint32_t)this->block[t * 4 + 0]) << 24) | (((uint32_t)this->block[t * 4 + 1]) << 16) |
+           (((uint32_t)this->block[t * 4 + 2]) << 8) | (((uint32_t)this->block[t * 4 + 3]) << 0);
   }
 
   for (t = 16; t < 64; t++) {
