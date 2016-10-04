6e6f85c2e5894ed3c9efdf7a37c518c74d80a02f
Matthew Slane <mcslane@iastate.edu>
2016-10-03 21:55:51 -0500

    documentation

--- MAC/src/MAC/types/Boolean.cs
+++ MAC/src/MAC/types/Boolean.cs
@@ -5,16 +5,25 @@ using System.Runtime.Serialization;
  namespace MAC.Types {
      /// <summary>
      /// Stores and vaildates boolean
-     /// Stored as SQL type bit or boolean
+     /// Stored as SQL type bit
      /// </summary>
      public class Boolean : BaseType {
         private bool Data;
 
+        /// <summary>
+        /// Initializes a Boolean from bool type
+        /// </summary>
+        /// <param name="input">the bool to be stored</param>
         public Boolean(bool input) {
             DatabaseFieldType = DatabaseFieldTypes.bit;
             Data = input;
         }
 
+        /// <summary>
+        /// Initializes a Boolean from SerializationInfo
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         public Boolean(SerializationInfo info, StreamingContext context) {
             if (info == null)
                 throw new ArgumentNullException("info");
@@ -31,10 +40,11 @@ using System.Runtime.Serialization;
         }
 
         /// <summary>
-        /// Compare booleans
+        /// Compare booleans (note that false < true)
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <param name="other">The BaseType object to compare with</param>
+        /// <returns>-1 if this is false and other is true, 0 if this==other,
+        /// and 1 if this is true and other is false</returns>
         public override int CompareTo(BaseType other) {
             if(other is Boolean) {
                 if (!this.Data && ((Boolean)other).Data) return -1;
@@ -47,8 +57,8 @@ using System.Runtime.Serialization;
         /// <summary>
         /// Checks equality of the booleans
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <param name="other">the BaseType object to check equality with</param>
+        /// <returns>true if equal, otherwise false</returns>
         public override bool Equals(BaseType other) {
             try {
                 return this.CompareTo(other) == 0;
@@ -59,7 +69,7 @@ using System.Runtime.Serialization;
         }
 
         /// <summary>
-        /// Should return the value of the boolean
+        /// Serialize the boolean
         /// </summary>
         /// <param name="info"></param>
         /// <param name="context"></param>
