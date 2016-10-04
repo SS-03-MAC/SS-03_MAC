1e9ca3a2f026be9b6fef835d5a61518b66283205
Matthew Slane <mcslane@iastate.edu>
2016-10-04 15:52:21 -0500

    documentation

--- MAC/src/MAC/types/Time.cs
+++ MAC/src/MAC/types/Time.cs
@@ -11,11 +11,20 @@ using System.Runtime.Serialization;
      public class Time : BaseType {
         private TimeSpan Data;
 
+        /// <summary>
+        /// initialized a Time from a TimeSpan object
+        /// </summary>
+        /// <param name="input">the TimeSpan to be stored</param>
         public Time(TimeSpan input) {
             Data = input;
             DatabaseFieldType = DatabaseFieldTypes.time;
         }
 
+        /// <summary>
+        /// initializes a Time from SerializationInfo
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         public Time(SerializationInfo info, StreamingContext context) {
             if (info == null)
                 throw new ArgumentNullException("info");
@@ -34,8 +43,8 @@ using System.Runtime.Serialization;
         /// <summary>
         /// Compare the time
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <param name="other">the BaseType object to compare with</param>
+        /// <returns>the result of TimeSpan.CompareTo()</returns>
         public override int CompareTo(BaseType other) {
             if (other is Time)
                 return this.Data.CompareTo(((Time)other).Data);
@@ -45,8 +54,8 @@ using System.Runtime.Serialization;
         /// <summary>
         /// Checks equality of the time
         /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
+        /// <param name="other">The BaseType object to check equalitry with</param>
+        /// <returns>True if equals, false otherwise</returns>
         public override bool Equals(BaseType other) {
             try {
                 return this.CompareTo(other) == 0;
@@ -56,7 +65,7 @@ using System.Runtime.Serialization;
         }
 
         /// <summary>
-        /// Should return the string
+        /// Serializes the Time
         /// </summary>
         /// <param name="info"></param>
         /// <param name="context"></param>
