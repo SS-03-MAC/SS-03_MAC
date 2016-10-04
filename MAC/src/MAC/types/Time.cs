725739fb377aa79a0e8b2dbb0b86d5960e6160dc
Matthew Slane <mcslane@iastate.edu>
2016-10-04 15:48:02 -0500

    detailed Time class

--- MAC/src/MAC/types/Time.cs
+++ MAC/src/MAC/types/Time.cs
@@ -9,13 +9,26 @@ using System.Runtime.Serialization;
      /// eg 08/28/2016
      /// </summary>
      public class Time : BaseType {
+        private TimeSpan Data;
+
+        public Time(TimeSpan input) {
+            Data = input;
+            DatabaseFieldType = DatabaseFieldTypes.time;
+        }
+
+        public Time(SerializationInfo info, StreamingContext context) {
+            if (info == null)
+                throw new ArgumentNullException("info");
+            Data = (TimeSpan)info.GetValue("Data", typeof(TimeSpan));
+        }
+
         /// <summary>
         /// This will check for:
         /// Will check if the time is format can interpered as a time
         /// </summary>
         /// <returns></returns>
         public override bool Validate() {
-            throw new NotImplementedException();
+            return true;
         }
 
         /// <summary>
@@ -24,7 +37,9 @@ using System.Runtime.Serialization;
         /// <param name="other"></param>
         /// <returns></returns>
         public override int CompareTo(BaseType other) {
-            throw new NotImplementedException();
+            if (other is Time)
+                return this.Data.CompareTo(((Time)other).Data);
+            throw new ArgumentException();
         }
 
         /// <summary>
@@ -33,7 +48,11 @@ using System.Runtime.Serialization;
         /// <param name="other"></param>
         /// <returns></returns>
         public override bool Equals(BaseType other) {
-            throw new NotImplementedException();
+            try {
+                return this.CompareTo(other) == 0;
+            } catch (ArgumentException) {
+                return false;
+            }
         }
 
         /// <summary>
@@ -42,7 +61,14 @@ using System.Runtime.Serialization;
         /// <param name="info"></param>
         /// <param name="context"></param>
         public override void GetObjectData(SerializationInfo info, StreamingContext context) {
-            throw new NotImplementedException();
+            if (info == null)
+                throw new ArgumentNullException("info");
+            info.AddValue("Data", Data);
+        }
+
+        public TimeSpan Value {
+            get { return Data; }
+            set { Data = value; }
         }
      }
  }
