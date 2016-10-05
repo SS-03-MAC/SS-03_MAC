5adc96946fd6d9d69c28924c8a419c8a601e1749
Matthew Burket <mburket@iastate.edu>
2016-10-05 18:27:52 -0500

    Updated Time tests

--- MAC/test/MAC.Test/Types/TimeTest.cs
+++ MAC/test/MAC.Test/Types/TimeTest.cs
@@ -6,10 +6,25 @@ namespace MAC.Test.Types
 {
     public class TimeTest
     {
+        [Fact]
         public void BasicTest()
         {
             Time time = new Time(new TimeSpan(20, 20, 20));
             Assert.True(time.Validate());
+        }
+
+        [Fact]
+        public void EqualToSelf()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Assert.True(time.Equals(time));
+        }
+
+        [Fact]
+        public void CompareToSelf()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Assert.Equal(0, time.CompareTo(time));
         }
     }
 }
