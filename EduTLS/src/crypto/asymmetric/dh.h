43e14ba223aae0f38b5212f3e50c845e40da3d5a
Alexander Scheel <scheel@iastate.edu>
2016-10-05 12:15:49 -0500

    [EduTLS] {dh} Implementation

---
+++ EduTLS/src/crypto/asymmetric/dh.h
@@ -0,0 +1,43 @@
+//===-- EduTLS/src/crypto/asymmetric/dh.h                -------*- C++ -*-===//
+//
+//                     EduTLS - Transport Layer Security
+//
+// This file is distributed under the MIT License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of the DH class for use with key
+/// exchange.
+///
+//===----------------------------------------------------------------------===//
+
+#include "../../bignum/bigint.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#pragma once
+
+class dh {
+private:
+  BigInt *x;
+  BigInt *y;
+  BigInt *p;
+  BigInt *q;
+  BigInt *g;
+  BigInt *h;
+  BigInt *j;
+
+public:
+  void compute(BigInt *output, BigInt *input);
+
+  void pub(BigInt *x);
+  void priv(BigInt *y);
+  void primes(BigInt *p, BigInt *q);
+  void generator(BigInt *g);
+
+  dh();
+  dh(BigInt *x, BigInt *y);
+  ~dh();
+};
