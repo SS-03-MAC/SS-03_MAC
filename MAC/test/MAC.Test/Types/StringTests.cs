eefdb521f0c5ef8ac6a6e4dac15f5c365b79b08a
Matthew Burket <mburket@iastate.edu>
2016-10-05 16:07:42 -0500

    Started tests and such

---
+++ MAC/test/MAC.Test/Types/StringTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+
+namespace MAC.Test.Types
+{
+    public class StringTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            Assert.IsNotType(String, null);
+        }
+
+    }
+}
