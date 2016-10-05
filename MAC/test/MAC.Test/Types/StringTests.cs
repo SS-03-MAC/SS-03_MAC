8ce86edbf71d5ca6dee8c65e6f97c4cb5ae98d12
Matthew Burket <mburket@iastate.edu>
2016-10-05 16:23:33 -0500

    Added some basic tests for string and type

--- MAC/test/MAC.Test/Types/StringTests.cs
+++ MAC/test/MAC.Test/Types/StringTests.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using Xunit;
+using MAC.Types;
 
-
-namespace MAC.Test.Types
+namespace MAC.Tests.Types
 {
     public class StringTests
     {
         [Fact]
         public void BasicTest()
         {
-            Assert.IsNotType(String, null);
+            MAC.Types.String s = new MAC.Types.String("apple");
+            Assert.True(s.Validate());
+        }
+
+        [Fact]
+        public void ToStringTest()
+        {
+            MAC.Types.String s = new MAC.Types.String("apple");
+            Assert.Equal("apple", s.ToString());
+        }
+
+        public void CompareEqual()
+        {
+            MAC.Types.String s = new MAC.Types.String("apple");
+            MAC.Types.String s2 = new MAC.Types.String("apple");
+            Assert.Equal(0, s.CompareTo(s2));
         }
 
     }
---
+++ MAC/test/MAC.Test/Types/TimeTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using MAC.Types;
+
+namespace MAC.Test.Types
+{
+    public class TimeTest
+    {
+        public void BasicTest()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Assert.True(time.Validate());
+        }
+    }
+}
