f67349b94b2fec564173164add9f981f629a3920
Matthew Burket <mb@matthewburket.com>
2016-10-05 12:38:00 -0500

    Added tests

---
+++ MAC/test/MAC.Test/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace MAC.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+        
+        [Fact]
+        public void Test2() 
+        {
+        	Assert.False(false);
+	}
+    }
+}
