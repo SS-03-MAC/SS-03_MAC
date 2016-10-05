09c2bcc0c37b02cac18f25732cbc09f376cf2ae6
Alexander Scheel <scheel@iastate.edu>
2016-10-05 14:59:09 -0500

    [EduTLS] {block} Implemented cbc, removed debug statements for ecb

--- EduTLS/src/crypto/block/cbc.h
+++ EduTLS/src/crypto/block/cbc.h
@@ -1,4 +1,4 @@
-//===-- EduTLS/src/crypto/block/ecb.h                     -------*- C++ -*-===//
+//===-- EduTLS/src/crypto/block/cbc.h                     -------*- C++ -*-===//
 //
 //                     EduTLS - Transport Layer Security
 //
@@ -7,11 +7,12 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file contains the implementation of ECB block mode with
+/// This file contains the implementation of CBC block mode with
 ///
 //===----------------------------------------------------------------------===//
 
 #include "../symmetric/symmetric.h"
+#include "../utils.h"
 #include <cstddef>
 #include <cstdint>
 #include <exception>
@@ -19,18 +20,22 @@
 
 #pragma once
 
-class ecb final : public symmetric {
+class cbc final : public symmetric {
 private:
   symmetric *cipher;
+  uint8_t *iv;
 
 public:
   inline int encrypt(uint8_t *output, uint8_t *input, size_t count) {
     if (count * 8 % this->cipher->block_size != 0) {
-      throw new std::invalid_argument("Invalid count for ECB mode.");
+      throw new std::invalid_argument("Invalid count for CBC mode.");
     }
 
+    uint8_t *prev = iv;
+
     for (unsigned long i = 0; i < count / (this->cipher->block_size / 8); i++) {
-      this->cipher->encrypt(output, input, this->cipher->block_size / 8);
+      edutls_xor(output, input, prev, this->cipher->block_size / 8);
+      this->cipher->encrypt(output, output, this->cipher->block_size / 8);
       output += this->cipher->block_size / 8;
       input += this->cipher->block_size / 8;
     }
@@ -39,11 +44,14 @@ public:
   };
   inline int decrypt(uint8_t *output, uint8_t *input, size_t count) {
     if (count * 8 % this->cipher->block_size != 0) {
-      throw new std::invalid_argument("Invalid count for ECB mode.");
+      throw new std::invalid_argument("Invalid count for CBC mode.");
     }
+
+    uint8_t *prev = iv;
 
     for (unsigned long i = 0; i < count / (this->cipher->block_size / 8); i++) {
       this->cipher->decrypt(output, input, this->cipher->block_size / 8);
+      edutls_xor(output, output, prev, this->cipher->block_size / 8);
       output += this->cipher->block_size / 8;
       input += this->cipher->block_size / 8;
     }
@@ -51,5 +59,8 @@ public:
     return 0;
   };
 
-  inline ecb(symmetric *cipher) { this->cipher = cipher; };
+  inline cbc(symmetric *cipher, uint8_t *iv) {
+    this->cipher = cipher;
+    this->iv = iv;
+  };
 };
