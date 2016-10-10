adc7baafcacb6f7dd7de5179f5937f1215fc8dcb
Matthew Burket <mb@matthewburket.com>
2016-10-10 16:27:46 -0500

    Added validations for email

--- MAC/src/MAC/types/Email.cs
+++ MAC/src/MAC/types/Email.cs
@@ -1,20 +1,33 @@
 using System;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 
-
- namespace MAC.Types.User {
+namespace MAC.Types.User {
      /// <summary>
      /// Stores and vaildates 
      /// </summary>
      public class Email : BaseType {
+        private string Data;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email">An email address</param>
+        public Email(string email)
+        {
+            Data = email;
+        }
+
+
         /// <summary>
         /// This will check for:
         /// - Vaild email address (a simple regex will work here)
-        /// - 
+        /// Regex for email valdations was taken from http://emailregex.com/
         /// </summary>
-        /// <returns></returns>
+        /// <returns>If the stored email is valid</returns>
         public override bool Validate() {
-            throw new NotImplementedException();
+            Regex EmailRegex = new Regex("[a - z0 - 9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+            return EmailRegex.IsMatch(Data);
         }
 
         /// <summary>
@@ -23,7 +36,11 @@ using System.Runtime.Serialization;
         /// <param name="other"></param>
         /// <returns></returns>
         public override int CompareTo(BaseType other) {
-            throw new NotImplementedException();
+            if (other is Email)
+            {
+
+            }
+            throw new ArgumentException();
         }
 
         /// <summary>
