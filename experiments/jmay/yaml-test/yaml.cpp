2b5a10ad060df84f22ed8f0a0b0dd217d47cbc57
Joel May <jmay@iastate.edu>
2016-10-10 21:36:23 -0700

    I don't even know

--- experiments/jmay/yaml-test/yaml.cpp
+++ experiments/jmay/yaml-test/yaml.cpp
@@ -1,7 +1,21 @@
 
 #include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <fstream>
 #include "yaml.h"
 
 int main(int argc, char *argv[]) {
-  YAML::Node n;
+    YAML::Node config = YAML::LoadFile("config.yaml");
+
+    //if (config["lastLogin"]) {
+        //std::cout << "Last logged in: " << config["lastLogin"].as<DateTime>() << "\n";
+    //}
+
+    const std::string username = config["username"].as<std::string>();
+    const std::string password = config["password"].as<std::string>();
+
+
+    std::ofstream fout("config.yaml");
+    fout << config;
+
 }
