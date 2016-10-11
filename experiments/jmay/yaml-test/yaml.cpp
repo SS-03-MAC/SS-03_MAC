6e3a2c913650bc8eb98885279e4214f169dd3794
Joel May <jmay@iastate.edu>
2016-10-11 00:03:10 -0500

    It builds on Ubuntu & Cygwin!

--- experiments/jmay/yaml-test/yaml.cpp
+++ experiments/jmay/yaml-test/yaml.cpp
@@ -5,7 +5,10 @@
 #include "yaml.h"
 
 int main(int argc, char *argv[]) {
+  try {
     YAML::Node config = YAML::LoadFile("config.yaml");
+
+
 
     //if (config["lastLogin"]) {
         //std::cout << "Last logged in: " << config["lastLogin"].as<DateTime>() << "\n";
@@ -17,5 +20,8 @@ int main(int argc, char *argv[]) {
 
     std::ofstream fout("config.yaml");
     fout << config;
+  } catch (const std::exception &ex) {
+    std::cout << "error" << ex.what() << std::endl;
+  }
 
 }
