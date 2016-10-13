2190c469ba9a1bbd963efd5bbb1d61841eda00f3
Alexander Scheel <scheel@iastate.edu>
2016-10-12 20:50:43 -0500

    [EduTLS] {ber} Tests for integers

--- EduTLS/tests/ber/tests.h
+++ EduTLS/tests/ber/tests.h
@@ -1,10 +1,13 @@
 #include "tests_boolean.h"
+#include "tests_integer.h"
 
 #include <cassert>
 #include <string>
 
 std::string tests_n[] = {
-    "Testing BER Boolean Encoding...", "Testing BER Boolean Decoding...",
+    "Testing BER Boolean Encoding...", "Testing BER Boolean Decoding...", "Testing BER Unsigned Integers...",
+    "Testing BER Signed Integers...",
 };
 
-void *tests_f[] = {(void *)&test_ber_boolean_encode, (void *)&test_ber_boolean_decode, NULL};
+void *tests_f[] = {(void *)&test_ber_boolean_encode, (void *)&test_ber_boolean_decode,
+                   (void *)&test_ber_unsigned_integer, (void *)&test_ber_signed_integer, NULL};
