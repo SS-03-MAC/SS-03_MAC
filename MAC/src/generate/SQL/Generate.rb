687ef1590f74fc1ba3eda7913d85d92a80689e0a
Matthew Burket <mburket@iastate.edu>
2016-10-17 16:51:13 -0500

    Work tests not fully working

--- MAC/src/generate/SQL/Generate.rb
+++ MAC/src/generate/SQL/Generate.rb
@@ -1,18 +1,21 @@
-module SSSaaS::Generate
-  'MAC.User.Password' = 'nvarchar',
-  'MAC.User.Email' = 'nvarchar',
-  'string' = 'nvarchar',
-  'Boolean' = 'bit',
-  'Currency' = 'money',
-  'Date' = 'time',
-  'DateTime' = 'datetime',
-  'Email' = 'nvarchar',
-  'Float' = 'float_t',
-  'MAC.Internet.IPAddress' = 'nvarchar',
-  'Integer' = 'int_t',
-  'MAC.Internet.MACAddress' = 'nvarchar',
-  'MAC.User.PhoneNummber' = 'nvarchar',
-  'Time' = 'time',
-  'Url' = 'nvarchar'
+module SSSaaS
+  class Generate
+  @types = {
+    'MAC.User.Password' => 'nvarchar',
+    'MAC.User.Email' => 'nvarchar',
+    'string' => 'nvarchar',
+    'Boolean' => 'bit',
+    'Currency' => 'money',
+    'Date' => 'time',
+    'DateTime' => 'datetime',
+    'Email' => 'nvarchar',
+    'Float' => 'float_t',
+    'MAC.Internet.IPAddress' => 'nvarchar',
+    'Integer' => 'int_t',
+    'MAC.Internet.MACAddress' => 'nvarchar',
+    'MAC.User.PhoneNummber' => 'nvarchar',
+    'Time' => 'time',
+    'Url' => 'nvarchar'
+  }
   end
 end
---
+++ MAC/src/generate/SQL/test/tc_sql_gen.rb
@@ -0,0 +1,13 @@
+require "test/unit"
+require 'erb'
+require './../Generate.rb'
+
+class TestSQLGen  < Test::Unit::TestCase
+  def basicTable
+    yaml = YAML.load(File.open(path))
+    @types = SSSaaS::Generate.types
+    ERB.new(File.read("../table.sql.erb")).result(yaml[0])
+    assert_equal(0,1)
+    assert(true)
+  end
+end
