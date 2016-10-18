05480e9003d218e80af4addc9998b85d3d42dd08
Alexander Scheel <scheel@iastate.edu>
2016-10-18 15:48:09 -0500

    [EduTLS] {ber} Tests for Printable String

--- EduTLS/tests/ber/tests.h
+++ EduTLS/tests/ber/tests.h
@@ -2,6 +2,7 @@
 #include "tests_ia5string.h"
 #include "tests_integer.h"
 #include "tests_octetstring.h"
+#include "tests_printablestring.h"
 #include "tests_utctime.h"
 
 #include <cassert>
@@ -18,6 +19,8 @@ std::string tests_n[] = {
     "Testing BER Long Octet String Decoding...",
     "Testing BER Short IA5 String Encoding...",
     "Testing BER Short IA5 String Decoding...",
+    "Testing BER Short Printable String Encoding...",
+    "Testing BER Short Printable String Decoding...",
     "Testing BER UTCTime Encoding...",
     "Testing BER UTCTime Decoding...",
 };
@@ -32,6 +35,8 @@ void *tests_f[] = {(void *)&test_ber_boolean_encode,
                    (void *)&test_ber_octetstring_long_decode,
                    (void *)&test_ber_ia5string_short_encode,
                    (void *)&test_ber_ia5string_short_decode,
+                   (void *)&test_ber_printablestring_short_encode,
+                   (void *)&test_ber_printablestring_short_decode,
                    (void *)&test_ber_utctime_encode,
                    (void *)&test_ber_utctime_decode,
                    NULL};
