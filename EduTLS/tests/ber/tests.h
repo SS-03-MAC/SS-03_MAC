4a5a8a946cc0e8df22013f6bc93582ad914cc790
Alexander Scheel <scheel@iastate.edu>
2016-10-17 23:10:26 -0500

    [EduTLS] {ber} Tests for bitstrings

--- EduTLS/tests/ber/tests.h
+++ EduTLS/tests/ber/tests.h
@@ -1,3 +1,4 @@
+#include "tests_bitstring.h"
 #include "tests_boolean.h"
 #include "tests_integer.h"
 
@@ -5,9 +6,15 @@
 #include <string>
 
 std::string tests_n[] = {
-    "Testing BER Boolean Encoding...", "Testing BER Boolean Decoding...", "Testing BER Unsigned Integers...",
-    "Testing BER Signed Integers...",
+    "Testing BER Boolean Encoding...",        "Testing BER Boolean Decoding...",
+    "Testing BER Unsigned Integers...",       "Testing BER Signed Integers...",
+    "Testing BER Short Bistring Encoding...", "Testing BER Short Bistring Decoding...",
 };
 
-void *tests_f[] = {(void *)&test_ber_boolean_encode, (void *)&test_ber_boolean_decode,
-                   (void *)&test_ber_unsigned_integer, (void *)&test_ber_signed_integer, NULL};
+void *tests_f[] = {(void *)&test_ber_boolean_encode,
+                   (void *)&test_ber_boolean_decode,
+                   (void *)&test_ber_unsigned_integer,
+                   (void *)&test_ber_signed_integer,
+                   (void *)&test_ber_bitstring_short_encode,
+                   (void *)&test_ber_bitstring_short_decode,
+                   NULL};
