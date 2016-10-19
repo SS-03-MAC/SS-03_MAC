6fada78c9535d7fea5339c9ec450c597dd47e2cf
Matthew Burket <mburket@iastate.edu>
2016-10-19 18:39:51 -0500

    Added attributes for models

---
+++ MAC/src/MAC/models/Attributes/DatabaseField.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAC.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class DatabaseField : Attribute
+    {
+        private string name;
+        
+        public DatabaseField(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ MAC/src/MAC/models/Attributes/Required.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MAC.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field|AttributeTargets.Property)]
+    public class Required : Attribute
+    {
+        private bool required;
+
+        public Required()
+        {
+            required = true;
+        }
+
+        public Required(bool required)
+        {
+            this.required = required;
+        }
+    }
+}
