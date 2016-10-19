56465b3366f0720d278dfd80b36ee1dc0af92216
Alexander Scheel <scheel@iastate.edu>
2016-10-19 16:22:19 -0500

    [EduTLS] {ber} Fixed Object Identifier

--- EduTLS/src/encoding/ber/objectidentifier.h
+++ EduTLS/src/encoding/ber/objectidentifier.h
@@ -27,15 +27,21 @@ inline size_t encode_objectidentifier_subidentifier(uint8_t *result, uint32_t va
     return 1;
   }
 
-  size_t p = 0;
-  while (value > 0) {
-    result[p] = (value & 0x7F) | 0x80;
-    p += 1;
-    value = value >> 7;
+  if (value < 0x3FFF) {
+    result[0] = (value >> 7) | 0x80;
+    result[1] = value & 0x7F;
+    return 2;
   }
-  result[p - 1] = value ^ 0x80;
 
-  return p;
+  if (value < 0xFFFFFFF) {
+    result[0] = ((value >> 21) & 0x7F) | 0x80;
+    result[1] = ((value >> 14) & 0x7F) | 0x80;
+    result[2] = ((value >> 7) & 0x7F) | 0x80;
+    result[3] = value & 0x7F;
+    return 4;
+  }
+
+  return 0;
 }
 
 inline size_t encode_objectidentifier_subidentifier_length(uint32_t value) {
@@ -48,6 +54,8 @@ inline size_t encode_objectidentifier_subidentifier_length(uint32_t value) {
     result += 1;
     value = value >> 7;
   }
+
+  return result;
 }
 
 inline size_t encode_objectidentifier_length(uint32_t *value, size_t length) {
@@ -63,7 +71,9 @@ inline size_t encode_objectidentifier_length(uint32_t *value, size_t length) {
     return 0;
   }
 
-  size_t result = encode_objectidentifier_subidentifier_length(40 * value[0] + value[1]);
+  size_t result = 1 + encode_length_length(length);
+
+  result += encode_objectidentifier_subidentifier_length(40 * value[0] + value[1]);
 
   for (size_t i = 2; i < length; i++) {
     result += encode_objectidentifier_subidentifier_length(value[i]);
@@ -85,7 +95,7 @@ inline void encode_objectidentifier(uint8_t *result, uint32_t *value, size_t len
     return;
   }
 
-  size_t size = encode_objectidentifier_length(value, length);
+  size_t size = encode_objectidentifier_length(value, length) - 1 - encode_length_length(length);
 
   result[0] = BER_IDENTIFIER_CLASS_UNIVERSAL | BER_IDENTIFIER_TYPE_PRIMITIVE | ASN_OBJECT_IDENTIFIER_CLASS;
   encode_length(&(result[1]), size);
@@ -126,6 +136,8 @@ inline size_t decode_objectidentifier_subidentifier_length(uint8_t *value) {
     result += 1;
   }
 
+  result += 1;
+
   return result;
 }
 
@@ -154,7 +166,7 @@ inline size_t decode_objectidentifier_length(uint8_t *value, size_t length) {
   }
 
   size_t data_pos = 0;
-  size_t result = 1;
+  size_t result = 2;
   for (data_pos = 0; data_pos < length - offset; data_pos++) {
     data_pos += decode_objectidentifier_subidentifier_length(&(value[offset + data_pos]));
     result += 1;
@@ -191,6 +203,7 @@ inline void decode_objectidentifier(uint32_t *result, uint8_t *value, size_t len
   size_t result_pos = 1;
   uint32_t data = 0;
   size_t data_length = 0;
+
   for (data_pos = 0; data_pos < value_length; data_pos++) {
     data_length = decode_objectidentifier_subidentifier_length(&(value[offset + data_pos]));
     data = decode_objectidentifier_subidentifier(&(value[offset + data_pos]), data_length);
@@ -210,7 +223,7 @@ inline void decode_objectidentifier(uint32_t *result, uint8_t *value, size_t len
       result[result_pos] = data;
     }
 
-    data_pos += data_length;
+    data_pos += data_length - 1;
     result_pos += 1;
   }
 }
