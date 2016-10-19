9cb9e9d88cfd59ea34546835189228df83f2df39
Alexander Scheel <scheel@iastate.edu>
2016-10-19 16:22:34 -0500

    [EduTLS] {ber} Testing for Object Identifier

--- EduTLS/tests/ber/tests.h
+++ EduTLS/tests/ber/tests.h
@@ -1,6 +1,7 @@
 #include "tests_boolean.h"
 #include "tests_ia5string.h"
 #include "tests_integer.h"
+#include "tests_objectidentifier.h"
 #include "tests_octetstring.h"
 #include "tests_printablestring.h"
 #include "tests_utctime.h"
@@ -21,6 +22,8 @@ std::string tests_n[] = {
     "Testing BER Short IA5 String Decoding...",
     "Testing BER Short Printable String Encoding...",
     "Testing BER Short Printable String Decoding...",
+    "Testing BER Object Identifier Encoding...",
+    "Testing BER Object Identifier Decoding...",
     "Testing BER UTCTime Encoding...",
     "Testing BER UTCTime Decoding...",
 };
@@ -37,6 +40,8 @@ void *tests_f[] = {(void *)&test_ber_boolean_encode,
                    (void *)&test_ber_ia5string_short_decode,
                    (void *)&test_ber_printablestring_short_encode,
                    (void *)&test_ber_printablestring_short_decode,
+                   (void *)&test_ber_objectidentifier_encode,
+                   (void *)&test_ber_objectidentifier_decode,
                    (void *)&test_ber_utctime_encode,
                    (void *)&test_ber_utctime_decode,
                    NULL};
