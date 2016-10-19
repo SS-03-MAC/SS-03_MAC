b9f20bfb9c6a5e221f91b026829034fbde3506d4
Matthew Burket <mburket@iastate.edu>
2016-10-19 17:27:25 -0500

    Move user model around

--- MAC/src/MAC/models/User.cs
+++ MAC/src/MAC/models/User.cs
@@ -2,31 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-/// <summary>
-///   Screensketch: Matthew Burket
-///   This is an example model class.
-///   I have subbed out the methods that will created in this file by the automatic system.
-/// </summary>
-public class User : BaseModel {
-  /// <summary>
-  /// Database id
-  /// </summary>
-  public long? Id { get; }
-  /// <summary>
-  /// A basic string field
-  /// </summary>
-  /// <returns></returns>
-  //[Required]
-  public string Email { get; set; }
-  /// <summary>
-  /// On set will set PasswordDigest to the BCrypt Password
-  /// </summary>
-  public virtual string Password { get; set; }
-  /// <summary>
-  /// How the Password is stored in the database
-  /// </summary>
-  /// <returns></returns>
-  public string PasswordDigest { get; set; }
-
-
+namespace MAC.Models
+{
+    public class User : BaseModel
+    {
+        /// <summary>
+        /// Database id
+        /// </summary>
+        public long? Id { get; }
+        /// <summary>
+        /// A basic string field
+        /// </summary>
+        /// <returns></returns>
+        //[Required]
+        public string Email { get; set; }
+        /// <summary>
+        /// On set will set PasswordDigest to the BCrypt Password
+        /// </summary>
+        public virtual string Password { get; set; }
+        /// <summary>
+        /// How the Password is stored in the database
+        /// </summary>
+        /// <returns></returns>
+        public string PasswordDigest { get; set; }
+    }
 }
