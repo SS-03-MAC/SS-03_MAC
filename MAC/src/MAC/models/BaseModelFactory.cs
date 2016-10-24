6f52757f1e050807b98c32e44c9fab4105b68d11
Matthew Burket <mburket@iastate.edu>
2016-10-24 18:46:17 -0500

    Rough draft of the BaseModelFactory

--- MAC/src/MAC/models/BaseModelFactory.cs
+++ MAC/src/MAC/models/BaseModelFactory.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using MAC.Models.Attributes;
+
 
 namespace MAC.Models
 {
     public static class BaseModelFactory
     {
-
+        public static object FillModel(Type model, IDataRecord reader)
+        {
+            var output = Activator.CreateInstance(model);
+            var properties = output.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Attribute dbField = property.GetCustomAttribute(Type.GetType("DatabaseField"));
+                if (dbField is DatabaseField)
+                {
+                    DatabaseField databaseField = (DatabaseField)property.GetCustomAttributes(typeof(DatabaseField)).FirstOrDefault();
+                    Type dataType = reader.GetFieldType(reader.GetOrdinal(databaseField.Name));
+                    var data = reader[databaseField.Name];
+                    Type[] constructorInfo = new Type[1];
+                    constructorInfo[1] = dataType;
+                    ConstructorInfo valueConstructor = dataType.GetConstructor(constructorInfo);
+                    object[] contrustorArgs = new object[1];
+                    contrustorArgs[0] = data;
+                    property.SetValue(output, valueConstructor.Invoke(contrustorArgs));
+                }
+            }
+            return output;
+        }
     }
 }
