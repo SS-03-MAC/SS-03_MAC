9643ccfc14afa2baea41b1d1e788ddb3da425efc
Matthew Burket <mburket@iastate.edu>
2016-11-01 20:56:49 -0500

    Updated Password type to match new base type

--- MAC/src/MAC/types/Password.cs
+++ MAC/src/MAC/types/Password.cs
@@ -8,7 +8,7 @@ namespace MAC.Types.User
     /// This the password class for user
     /// This class will handle 
     /// </summary>
-    public class Password : BaseType
+    public class Password : BaseType<Password, string>
     {
         private string Data;
         /// <summary>
@@ -42,7 +42,7 @@ namespace MAC.Types.User
         /// </summary>
         /// <param name="other"></param>
         /// <returns></returns>
-        public override int CompareTo(BaseType other)
+        public override int CompareTo(object other)
         {
             throw new NotImplementedException();
         }
@@ -61,7 +61,7 @@ namespace MAC.Types.User
         /// </summary>
         /// <param name="other"></param>
         /// <returns></returns>
-        public override bool Equals(BaseType other)
+        public override bool Equals(object other)
         {
             throw new NotImplementedException();
         }
@@ -72,7 +72,11 @@ namespace MAC.Types.User
         /// <returns></returns>
         public override void GetObjectData(SerializationInfo info, StreamingContext cont)
         {
-            throw new NotImplementedException();
+            if (info == null)
+            {
+                throw new ArgumentNullException("info");
+            }
+            info.AddValue("Data", Data);
         }
 
         /// <summary>
@@ -93,6 +97,12 @@ namespace MAC.Types.User
         public bool VerifyPassword(string PlainTextPassword)
         {
             return BCrypt.Net.BCrypt.Verify(PlainTextPassword, Data);
+        }
+
+        public override string Value
+        {
+            get { return Data; }
+            set { Data = Value; }
         }
     }
 }
