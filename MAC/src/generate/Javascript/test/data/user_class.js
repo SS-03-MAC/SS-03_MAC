b36c4aae2ea4d226860caddc623659cc421e4616
Matthew Slane <mcslane@iastate.edu>
2016-11-01 23:13:04 -0500

    finished GET/id request

--- MAC/src/generate/Javascript/test/data/user_class.js
+++ MAC/src/generate/Javascript/test/data/user_class.js
@@ -11,11 +11,16 @@ var User = function(name, uses, fields){
 User.get = function(id){
   xhr = new XMLHTTPRequest();
   xhr.onreadystatechenge = function() {
-    if(this.readystate == 4 && this.status == 200){
-      //do something
+    if(this.readystate == 4){
+      if(this.status == 200){
+        var result = eval("("+this.responseTest+")");
+        return result;
+      } else{
+        throw new Error(this.statusText);
+      }
     }
   }
 
   xhr.open("GET", /users/id, true);
-  xhr.send();
+  xhr.send(null);
 }
