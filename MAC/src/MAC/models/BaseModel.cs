10a15ab925390e112a0f2159b66a0b14147b948d
Matthew Burket <mburket@iastate.edu>
2016-11-03 14:45:29 -0500

    Reworked BaseModel so that static methods can get access to the TableName

--- MAC/src/MAC/models/BaseModel.cs
+++ MAC/src/MAC/models/BaseModel.cs
@@ -4,19 +4,63 @@ using MAC.Types;
 using MAC.Models.Attributes;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace MAC.Models
 {
     /// <summary>
     /// Base model that all MAC models should be dertived from
     /// </summary>
-    public abstract class BaseModel : IBaseModel
+    public abstract class BaseModel<T> : BaseModel
+    {
+        /// <summary>
+        ///  Blank contrustor
+        /// </summary>
+        public BaseModel()
+        {
+
+        }
+
+        /// <summary>
+        ///  Delete the current record
+        /// </summary>
+        /// <returns></returns>
+        public bool Delete()
+        {
+            if (Id == null)
+            {
+                throw new InvalidOperationException("Cannot delete a record that is not saved to the database");
+            }
+            return Delete(Id.Value);
+
+        }
+
+        private static string GetStaticTableName()
+        {
+
+            return (typeof(T).GetTypeInfo().GetCustomAttribute(typeof(TableName)) as TableName).Name;
+        }
+
+
+        /// <summary>
+        /// Deletes the given record with the given id
+        /// </summary>
+        /// <param name="id">If the record was deleted</param>
+        /// <returns></returns>
+        public static bool Delete(int id)
+        {
+            return Query.Delete(GetStaticTableName(), id);
+            return false;
+        }
+    }
+
+    public class BaseModel : IBaseModel
     {
         /// <summary>
         /// SQL Indentify column 
         /// </summary>
         /// <returns></returns>
-        [DatabaseField("ID")]
+        [DatabaseField("Id")]
         public Integer Id { get; set; }
 
         /// <summary>
@@ -32,14 +76,6 @@ namespace MAC.Models
         /// <returns></returns>
         [DatabaseField("UpdatedAt")]
         public Types.DateTime UpdatedAt { get; set; }
-
-        /// <summary>
-        ///  Blank contrustor
-        /// </summary>
-        public BaseModel()
-        {
-
-        }
 
         /// <summary>
         /// Saves the record to the database
@@ -80,43 +116,45 @@ namespace MAC.Models
             return false;
         }
 
+
         /// <summary>
-        ///  Delete the current record
+        /// Converts the current record to an update statement
         /// </summary>
         /// <returns></returns>
-        public bool Delete()
+        public SqlCommand ToUpdateStatement()
         {
-            if (Id == null)
+            string result = "UPDATE " + GetTableName() + " SET ";
+            foreach (KeyValuePair<string, BaseType> kvPair in GetDatabaseFields())
             {
-                throw new InvalidOperationException("Cannot delete a record that is not saved to the database");
+                if (kvPair.Key.ToLower() == "id")
+                {
+                    continue;
+                }
+                result += kvPair.Key + "=@" + kvPair.Key + ",";
             }
-            return Delete(Id.Value);
-
+            result = result.Remove(result.LastIndexOf(","));
+            result += " WHERE Id = " + Id.Value + ";";
+            return  new SqlCommand(result);
         }
 
         /// <summary>
-        /// Converts the current record to an Insert statement
-        /// 
-        /// This method is public for testing purposes
+        /// Get the database properities of this model in a useable format
         /// </summary>
-        public SqlCommand ToInsertStatement()
+        /// <returns></returns>
+        private List<KeyValuePair<string, BaseType>> GetDatabaseFields()
         {
-            string query = "INSERT INTO " + GetTableName();
-            List<KeyValuePair<string, BaseType>> values = GetDatabaseFields();
-            query += " (";
-            query = GetColumnNames(query, values);
-            query += ") VALUES (";
-            query = GetValuesForInsert(query, values);
-            query += ")";
-            query += ";";
-            SqlCommand result = new SqlCommand(query);
-            SetValues(values, result);
+            List<KeyValuePair<string, BaseType>> result = new List<KeyValuePair<string, BaseType>>();
+            foreach (PropertyInfo property in GetType().GetProperties())
+            {
+                Attribute dbField = property.GetCustomAttribute(typeof(DatabaseField));
+                if (dbField is DatabaseField)
+                {
+                    string columnName = ((DatabaseField)dbField).Name;
+                    KeyValuePair<string, BaseType> value = new KeyValuePair<string, BaseType>(columnName, (BaseType)property.GetValue(this));
+                    result.Add(value);
+                }
+            }
             return result;
-        }
-
-        private string GetTableName()
-        {
-            return (GetType().GetTypeInfo().GetCustomAttribute(typeof(TableName)) as TableName).Name;
         }
 
         private static void SetValues(List<KeyValuePair<string, BaseType>> values, SqlCommand result)
@@ -127,6 +165,12 @@ namespace MAC.Models
             }
         }
 
+        /// <summary>
+        /// Get the values for the insert query and adds then to the query
+        /// </summary>
+        /// <param name="query">The insert query to add the balues to </param>
+        /// <param name="values">The KeyValue store that has the values</param>
+        /// <returns></returns>
         private static string GetValuesForInsert(string query, List<KeyValuePair<string, BaseType>> values)
         {
             foreach (KeyValuePair<string, BaseType> key in values)
@@ -162,54 +206,34 @@ namespace MAC.Models
         }
 
         /// <summary>
-        /// Converts the current record to an update statement
+        /// Converts the current record to an Insert statement
+        /// 
+        /// This method is public for testing purposes
         /// </summary>
-        /// <returns></returns>
-        public string ToUpdateStatement()
+        /// <returns>A SQLCommand with command text as an insert statement for insert the record into the statement</returns>
+        public SqlCommand ToInsertStatement()
         {
-            string result = "UPDATE " + GetTableName() + " SET ";
-            foreach(KeyValuePair<string, BaseType> kvPair in GetDatabaseFields())
-            {
-                if (kvPair.Key.ToLower() == "id")
-                {
-                    continue;
-                }
-                result += kvPair.Key + "=@" + kvPair.Key + ","; 
-            }
-            result = result.Remove(result.LastIndexOf(","));
-            result += " WHERE Id = " + Id.Value + ";";
+            string query = "INSERT INTO " + GetTableName();
+            List<KeyValuePair<string, BaseType>> values = GetDatabaseFields();
+            query += " (";
+            query = GetColumnNames(query, values);
+            query += ") VALUES (";
+            query = GetValuesForInsert(query, values);
+            query += ")";
+            query += ";";
+            SqlCommand result = new SqlCommand(query);
+            SetValues(values, result);
             return result;
         }
 
         /// <summary>
-        /// Deletes the given record with the given id
+        /// The name table returned as string
         /// </summary>
-        /// <param name="id">If the record was deleted</param>
-        /// <returns></returns>
-        public static bool Delete(int id)
+        /// <returns>Talbe name as as string</returns>
+        private string GetTableName()
         {
-            //return Query.Delete(GetTableName(), id);
-            return false;
-        }
 
-        /// <summary>
-        /// Get the database properities of this model in a useable format
-        /// </summary>
-        /// <returns></returns>
-        private List<KeyValuePair<string, BaseType>> GetDatabaseFields()
-        {
-            List<KeyValuePair<string, BaseType>> result = new List<KeyValuePair<string, BaseType>>();
-            foreach (PropertyInfo property in GetType().GetProperties())
-            {
-                Attribute dbField = property.GetCustomAttribute(typeof(DatabaseField));
-                if (dbField is DatabaseField)
-                {
-                    string columnName = ((DatabaseField)dbField).Name;
-                    KeyValuePair<string, BaseType> value = new KeyValuePair<string, BaseType>(columnName, (BaseType) property.GetValue(this));
-                    result.Add(value);
-                }
-            }
-            return result;
+            return (GetType().GetTypeInfo().GetCustomAttribute(typeof(TableName)) as TableName).Name;
         }
     }
 }
