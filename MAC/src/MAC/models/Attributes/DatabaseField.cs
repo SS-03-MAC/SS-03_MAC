6312711faf4035b81ae049c918150b97bfbd980f
Matthew Burket <mburket@iastate.edu>
2016-11-03 11:05:23 -0500

    Added docs for Database Field Attribute

--- MAC/src/MAC/models/Attributes/DatabaseField.cs
+++ MAC/src/MAC/models/Attributes/DatabaseField.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MAC.Models.Attributes
 {
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
     public class DatabaseField : Attribute
     {
+        /// <summary>
+        /// Name of the table
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Create attribute with a name
+        /// </summary>
+        /// <param name="name">Name of database field</param>
         public DatabaseField(string name)
         {
             Name = name;
