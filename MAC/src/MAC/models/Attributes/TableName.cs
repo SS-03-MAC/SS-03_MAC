5caa5d2fe86b087e861aa950d46a9fc15afdff97
Matthew Burket <mburket@iastate.edu>
2016-11-03 11:04:47 -0500

    Added TableName Attribute

---
+++ MAC/src/MAC/models/Attributes/TableName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MAC.Models.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class TableName : Attribute
+    {
+        /// <summary>
+        /// Name of table
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Create Attribute with a name
+        /// </summary>
+        /// <param name="Name">Name of table</param>
+        public TableName(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
