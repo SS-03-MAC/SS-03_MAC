bf5c4efe32dad7bbf35288f9aeb96205256e4217
Alexander Scheel <scheel@iastate.edu>
2016-11-06 10:14:12 -0600

    [EduTLS] {tests} HMAC SHA256 test from OpenSSL packet contents

--- EduTLS/tests/hmac/tests.h
+++ EduTLS/tests/hmac/tests.h
@@ -5,8 +5,8 @@
 
 std::string tests_n[] = {
     "Testing HMAC MD5 with first test vector...", "Testing HMAC MD5 with second test vector...",
-    "Testing HMAC MD5 with third test vector...",
+    "Testing HMAC MD5 with third test vector...", "Testing HMAC SHA256 from actual data...",
 };
 
 void *tests_f[] = {(void *)&test_hmac_md5_vector_one, (void *)&test_hmac_md5_vector_two,
-                   (void *)&test_hmac_md5_vector_three, NULL};
+                   (void *)&test_hmac_md5_vector_three, (void *)&test_hmac_sha256, NULL};
