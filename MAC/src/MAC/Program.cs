496f69a06826ab1f3a67a4e29e5a56271f7cd303
Matthew Burket <mburket@iastate.edu>
2016-11-09 18:50:41 -0600

    Started work on dymaic getting of models

--- MAC/src/MAC/Program.cs
+++ MAC/src/MAC/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using System.Reflection;
+using MAC.Models.Attributes;
 
 namespace ConsoleApplication
 {
@@ -8,6 +13,9 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+          Type t = GetTypeFromString("users");
+            string contentString = string.Empty; //GetFormContentets();
+
             string requestMethod = Environment.GetEnvironmentVariable("REQUEST_METHOD");
             if (requestMethod == "GET")
             {
@@ -20,8 +28,26 @@ namespace ConsoleApplication
             {
 
             }
-            
+
 
         }
+
+        public static Type GetTypeFromString(string urlName)
+        {
+            Type[] klasses = Assembly.GetEntryAssembly().GetTypes();
+            foreach (Type t in klasses)
+            {
+                var attributes = t.GetTypeInfo().GetCustomAttributes<UrlName>();
+                foreach (UrlName urlName2 in attributes)
+                {
+                    if (urlName2.Url == urlName)
+                    {
+                        return t;
+                    }
+                }
+            }
+            return null;
+        }
+
     }
 }
