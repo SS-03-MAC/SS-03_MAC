32c2901af84c931b508afba9d8273e857acd5023
Matthew Burket <mburket@iastate.edu>
2016-11-14 16:43:27 -0600

    Added new tests for Date Type

--- MAC/test/MAC.Test/Types/DateTests.cs
+++ MAC/test/MAC.Test/Types/DateTests.cs
@@ -27,5 +27,13 @@ namespace MAC.Test.Types
             Date d2 = new Date(new System.DateTime(2016, 11, 02));
             Assert.True(d.Equals(d2));
         }
+
+        [Fact]
+        public void notEqual()
+        {
+            Date d = new Date(new System.DateTime(2016, 11, 02));
+            Date d2 = new Date(new System.DateTime(2018, 11, 02));
+            Assert.False(d.Equals(d2));
+        }
     }
 }
