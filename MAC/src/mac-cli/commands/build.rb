eac93ccacd74a8a8c342c314d3759cb4f59785bb
Matthew Slane <mcslane@iastate.edu>
2016-11-14 16:28:43 -0600

    finished flag parsing

--- MAC/src/mac-cli/commands/build.rb
+++ MAC/src/mac-cli/commands/build.rb
@@ -20,20 +20,30 @@ module MacCLI
     end
 
     def build_js(path)
+      puts "doing js"
     end
 
     def build_cs(path)
+      puts "doing cs"
     end
 
     def build_sql(path)
+      puts "doing SQL"
     end
     
     def option_parser(args)
+      flags = {}
       opt_parser = OptionParser.new do |opts|
         opts.banner = 'Useage: mac-cli build [options]'
-        opts.on('--no-javascript', '--no-js', 'Does not update JavaScript models')
-        opts.on('--no-sql', 'Does not update SQL table')
-        opts.on('--no-cs', 'Does not update C# models')
+        opts.on('--no-javascript', '--no-js', 'Does not update JavaScript models') do |v|
+          flags['js'] = false
+        end
+        opts.on('--no-sql', 'Does not update SQL table') do |v|
+          flags['sql'] = false
+        end
+        opts.on('--no-cs', 'Does not update C# models') do |v|
+          flags['js'] = false
+        end
         opts.on('--no-api', 'Does not update API definitions')
         opts.on('-h', '--help', 'Shows this help message.') do |v|
           puts opt_parser 
@@ -42,6 +52,7 @@ module MacCLI
       end
 
       opt_parser.parse!(args)
+      flags
     end
   end
 end
