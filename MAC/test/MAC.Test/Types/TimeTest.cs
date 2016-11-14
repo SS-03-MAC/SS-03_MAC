4c8625e247344839679e8664aed3c0a828d23def
Matthew Burket <mburket@iastate.edu>
2016-11-14 16:45:02 -0600

    Added new tests for TimeType

--- MAC/test/MAC.Test/Types/TimeTest.cs
+++ MAC/test/MAC.Test/Types/TimeTest.cs
@@ -21,10 +21,34 @@ namespace MAC.Test.Types
         }
 
         [Fact]
+        public void Equal()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Time time2 = new Time(new TimeSpan(20, 20, 20));
+            Assert.True(time2.Equals(time));
+        }
+
+        [Fact]
+        public void NotEqual()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Time time2 = new Time(new TimeSpan(20, 20, 10));
+            Assert.False(time2.Equals(time));
+        }
+
+        [Fact]
         public void CompareToSelf()
         {
             Time time = new Time(new TimeSpan(20, 20, 20));
             Assert.Equal(0, time.CompareTo(time));
+        }
+
+        [Fact]
+        public void CompareToSame()
+        {
+            Time time = new Time(new TimeSpan(20, 20, 20));
+            Time time2 = new Time(new TimeSpan(20, 20, 20));
+            Assert.Equal(0, time.CompareTo(time2));
         }
     }
 }
