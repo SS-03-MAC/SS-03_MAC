d22c2203457336998d8383b7c1ef7a929e6d8f9d
Matthew Burket <mburket@iastate.edu>
2016-11-15 22:40:59 -0600

    Rework lengths

---
+++ MAC/src/MAC/models/Attributes/Validations/MaxLength.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MAC.Models.Attributes.Validations
+{
+    /// <summary>
+    /// Set a min and/or max length
+    /// </summary>
+    public class MaxLength : BaseValidation
+    {
+        /// <summary>
+        /// Maxiumim length
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Allow blank, only check length if there is content
+        /// </summary>
+        public bool AllowBlank { get; set; }
+
+        public MaxLength(int length)
+        {
+            Length = length;
+        }
+
+        /// <summary>
+        /// Override of <see cref="BaseValidation.IsValid(object)"/> 
+        /// </summary>
+        /// <param name="o">object to test</param>
+        /// <returns>
+        ///  <c>false</c> if the given value's length is too long
+        ///  <c>true</c> if the given value's length is shorter or equal to the max length
+        /// </returns>
+        public override bool IsValid(object o)
+        {
+            CheckLenth();
+
+            // RequiredField is be used for null check
+            if (o == null)
+            {
+                return true;
+            }
+
+            if (Length != 0 && o == null && AllowBlank)
+            {
+                return false;
+            }
+
+            string value = o as string;
+
+            if (value != null && value.Length < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if the valdations has a valid length
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If length is zero or less</exception>
+        private void CheckLenth()
+        {
+            if (Length <= 0)
+            {
+              throw new  InvalidOperationException("Length must be > 0");
+            }
+        }
+    }
+}
---
+++ MAC/src/MAC/models/Attributes/Validations/MinLength.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAC.Models.Attributes.Validations
+{
+    public class MinLength : BaseValidation
+    {
+        /// <summary>
+        /// Specifies the minimum length of collection/string data allowed in a property.
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Override of  <see cref="BaseValidation.IsValid(object)"/> 
+        /// </summary>
+        /// <param name="o">object to test</param>
+        /// <returns>
+        /// <c>false</c> If the given lenght is less than min length
+        /// <c>true</c> If the given object is longer
+        /// </returns>
+        public override bool IsValid(object o)
+        {
+            CheckLength();
+            // RequiredField is be used for null check
+            if (o == null)
+            {
+                return true;
+            }
+
+            if (o == null)
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the valdations has a valid length
+        /// </summary>
+        /// <exception cref="InvalidOperationException">If length is zero or less</exception>
+        private void CheckLenth()
+        {
+            if (Length <= 0)
+            {
+                throw new InvalidOperationException("Length must be > 0");
+            }
+        }
+    }
+}
