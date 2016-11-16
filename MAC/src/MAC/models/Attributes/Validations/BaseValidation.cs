d200dd26b903421b9d35434138538f5c6abd9aaa
Matthew Burket <mburket@iastate.edu>
2016-11-15 22:42:00 -0600

    Added docs and IsValid method

--- MAC/src/MAC/models/Attributes/Validations/BaseValidation.cs
+++ MAC/src/MAC/models/Attributes/Validations/BaseValidation.cs
@@ -5,7 +5,21 @@ using System.Threading.Tasks;
 
 namespace MAC.Models.Attributes.Validations
 {
-    public class BaseValidation : Attribute
+    /// <summary>
+    /// Base atttrbiute that all validations should be derivted from
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Parameter,
+        AllowMultiple = false)]
+    public abstract class BaseValidation : Attribute
     {
+        /// <summary>
+        /// Validate a given object
+        /// </summary>
+        /// <param name="o">object to validate</param>
+        /// <returns>
+        /// <c>false</c> if the object is not valid
+        /// <c>true</c> if the object is valid
+        /// </returns>
+        public abstract bool IsValid(object o);
     }
 }
