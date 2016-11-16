9bc2dbddaf9ab81ce1be13b1d2b62ffb3b82c62b
Matthew Burket <mburket@iastate.edu>
2016-11-16 11:28:05 -0600

    Docs and changes to built in validation on Required validaiton

--- MAC/src/MAC/models/Attributes/Validations/Required.cs
+++ MAC/src/MAC/models/Attributes/Validations/Required.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MAC.Types;
+using System;
 
 namespace MAC.Models.Attributes.Validations
 {
@@ -15,19 +16,17 @@ namespace MAC.Models.Attributes.Validations
 
         /// <summary>
         /// Override of <see cref="BaseValidation.IsValid(object)"/> 
+        /// Underline check uses <see cref="System.ComponentModel.DataAnnotations.RequiredAttribute"/>.
         /// </summary>
-        /// <param name="o"></param>
-        /// <returns></returns>
-        public override bool IsValid(object o)
+        /// <param name="o">object to check</param>
+        /// <returns>
+        /// <c>false</c> If emtpy 
+        /// <c>true</c> If Defined
+        /// </returns>
+        public override bool IsValid(BaseType o)
         {
-            if (o is MAC.Types.String)
-            {
-                MAC.Types.String a = (MAC.Types.String) o;
-                o = a.Value as object;
-            }
-
             var attr = new System.ComponentModel.DataAnnotations.RequiredAttribute();
-            return attr.IsValid(o);
+            return attr.IsValid(o.GetRawObject());
         }
     }
 }
