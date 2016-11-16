ca95ba624c6cb899fef49e813a97920f28bb4b2e
Matthew Slane <mcslane@iastate.edu>
2016-11-16 16:40:44 -0600

    added documentation

--- MAC/src/mac-cli/commands/build.rb
+++ MAC/src/mac-cli/commands/build.rb
@@ -5,11 +5,13 @@ require_relative './../../generate/CSharp/generate_csharp.rb'
 require_relative './../../parsing/parser.rb'
 
 module MacCLI
-  ##
-  # Build for MacCLI
+  # Build for MacCLI. Generates all models based off of the yaml
+  # provided. has options for not doing certain languages.
   class Build
     include Generation
     include Parser
+
+    # builds the things using provieed arguments and paths for things to go to
     def build(args, js_path, cs_path, sql_path, yml_path)
       opts = option_parser(args)
       model_hash = Parser.parse_dir(yml_path)
@@ -26,6 +28,7 @@ module MacCLI
       end
     end
 
+    # generates the JavaScript frontend endpoints for the provided models
     def build_js(path, models)
       gen = GenerateJavascript.new
       models.each_value do |type_list|
@@ -36,26 +39,29 @@ module MacCLI
       end
     end
 
+    # genrates the C# backend endpoint for the provided models
     def build_cs(path, models)
       gen = GenerateCSharp.new
       models.each_value do |type_list|
         type_list.each do |type|
           model_name = type['name']
-          gen.write_class(type, path + model_name + '.cs')
+          gen.write_model(type, path + model_name + '.cs')
         end
       end
     end
 
+    # generates the sql files for the provided models
     def build_sql(path, models)
       gen = GenerateSQL.new
       models.each_value do |type_list|
         type_list.each do |type|
           model_name = type['name']
-          gen.write_class(type, path + model_name + '.cs')
+          gen.write_table(type, path + model_name + '.sql')
         end
       end
     end
     
+    # parses options. currently does stuff for no javascript, no sql, no cs, and help
     def option_parser(args)
       flags = {}
       opt_parser = OptionParser.new do |opts|
@@ -69,7 +75,6 @@ module MacCLI
         opts.on('--no-cs', 'Does not update C# models') do |v|
           flags['js'] = false
         end
-        opts.on('--no-api', 'Does not update API definitions')
         opts.on('-h', '--help', 'Shows this help message.') do |v|
           puts opt_parser 
           exit
