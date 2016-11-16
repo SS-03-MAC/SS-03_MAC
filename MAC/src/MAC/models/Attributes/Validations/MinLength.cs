f24661391ee9c3ad6280668fee423ac71b6ae979
Matthew Burket <mburket@iastate.edu>
2016-11-16 11:26:27 -0600

    Rework docs, changed to BaseType for is valid, Added contrustor for min length validtion

--- MAC/src/MAC/models/Attributes/Validations/MinLength.cs
+++ MAC/src/MAC/models/Attributes/Validations/MinLength.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MAC.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MAC.Models.Attributes.Validations
 {
+    /// <summary>
+    /// Checks if there is a min length meet
+    /// </summary>
     public class MinLength : BaseValidation
     {
         /// <summary>
@@ -12,18 +16,24 @@ namespace MAC.Models.Attributes.Validations
         /// </summary>
         public int Length { get; set; }
 
+        public MinLength(int length)
+        {
+            Length = length;
+        }
+
         /// <summary>
-        /// Override of  <see cref="BaseValidation.IsValid(object)"/> 
+        /// Override of  <see cref="BaseValidation.IsValid(object)"/>
+        /// Underlaying check uses <see cref="System.ComponentModel.DataAnnotations.MinLengthAttribute"/> 
         /// </summary>
         /// <param name="o">object to test</param>
         /// <returns>
         /// <c>false</c> If the given lenght is less than min length
         /// <c>true</c> If the given object is longer
         /// </returns>
-        public override bool IsValid(object o)
+        public override bool IsValid(BaseType o)
         {
             var attr = new System.ComponentModel.DataAnnotations.MinLengthAttribute(Length);
-            return attr.IsValid(o);
+            return attr.IsValid(o.GetRawObject());
           
         }
 
