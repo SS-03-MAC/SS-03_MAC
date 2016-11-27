82d4c6b74ae677f880fc7a9d28404f91220382c8
Matthew Slane <mcslane@iastate.edu>
2016-11-27 14:08:25 -0600

    updated build paths

--- MAC/src/mac-cli/mac_cli.rb
+++ MAC/src/mac-cli/mac_cli.rb
@@ -9,19 +9,19 @@ module MacCLI
   # Modules
   # TODO: Make this more rubish
   if ARGV[0] == 'build'
-    require './commands/build'
+    require_relative './commands/build'
     ARGV.shift
     build = Build.new
-    build.build(ARGV, './models/js/', './models/csharp/', './models/sql/', './yaml')
+    build.build(ARGV, './public/', './app/', './db/', './config/models/')
     exit
   elsif ARGV[0] == 'init'
-    require './commands/init'
+    require_relative './commands/init'
     ARGV.shift
     init = Init.new
     init.init(ARGV)
     exit
   elsif ARGV[0] == 'run'
-    require './commands/run'
+    require_relative './commands/run'
     ARGV.shift
     run = Run.new
     run.option_parser(ARGV)
